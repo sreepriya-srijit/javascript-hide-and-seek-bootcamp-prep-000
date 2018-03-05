81879fec509f471888ae166a3e8a6b80e759d3b9
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-03-05 23:23:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector)
+{
+  const element =  document.getElementById('app').querySelector(selector);
+  return element;
+}
+
+ function nestedTarget(){
+   const element = document.querySelector('#nested .target');
+   return element;
+ } 
+ 
+ function deepestChild(){
+   return document.querySelector('#grand-node div div div div');
+ }
+ 
+ function increaseRankBy(n){
+    const element =  document.getElementById('app').querySelectorAll('.ranked-list');
+    for (let i = 0; i < element.length; i++) {
+      element[i].innerHTML= parseInt( element[i].innerHTML) + n;
+    }
+ }
