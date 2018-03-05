6e05c9677bad5fbd183d20025a327381c57d60c3
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-03-05 23:27:21 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ function getFirstSelector(selector)
  } 
  
  function deepestChild(){
-   return document.querySelector('#grand-node div div div div');
+  // return document.querySelector('#grand-node div div div div');
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+
  }
  
  function increaseRankBy(n){
